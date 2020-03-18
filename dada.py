d69893319101d709015ff96f3192f47aa375788c
yrlp98 <yousef.r1999@gmail.com>
2020-03-19 01:59:31 +0330

    Add ping command

--- dada.py
+++ dada.py
@@ -1,12 +1,19 @@
 import discord
 from discord.ext import commands
 
-token = '/* Add your toekn here */'
-client = commands.Bot(command_prefix='!')
+token = 'Njg5NTM1MjAwNzQwMjQ1NjIy.XnJmIg.vVSX0t31rqlDzBUwVqnemZyIJSw'
+client = commands.Bot(command_prefix='~')
 
-
+# !to check bot is up or not
 @client.event
 async def on_ready():
     print('Bot is ready!')
+
+# !the bot commands
+# show user ping
+@client.command()
+async def ping(ctx):
+    await ctx.send(f':stopwatch: {round(client.latency * 1000)}ms')
+
 
 client.run(token)
