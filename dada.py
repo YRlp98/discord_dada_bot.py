7c8d32917773c91be7cc1554ea9879c43361cb04
yrlp98 <yousef.r1999@gmail.com>
2020-03-19 02:29:40 +0330

    Add estekhare command

--- dada.py
+++ dada.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 
 token = 'Njg5NTM1MjAwNzQwMjQ1NjIy.XnJmIg.vVSX0t31rqlDzBUwVqnemZyIJSw'
@@ -10,10 +11,20 @@ async def on_ready():
     print('Bot is ready!')
 
 # !the bot commands
+
 # show user ping
-@client.command()
+@client.command(aliases=['Ping'])
 async def ping(ctx):
     await ctx.send(f':stopwatch: {round(client.latency * 1000)}ms')
+
+# Response with a random awnseer
+@client.command(aliases=['Estekhare', 'soal', 'Soal'])
+async def estekhare(ctx, *, question):
+    responses = [
+        'gi nakhor dada!',
+        'gi bokhor dada'
+    ]
+    await ctx.send(f'dar javabe solae: {question}\nbayad begam ke: {random.choice(responses)}')
 
 
 client.run(token)
