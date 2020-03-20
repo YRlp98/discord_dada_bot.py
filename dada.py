ed3b2385f53c9a7d004cb75dd532a5f0d4612d12
yrlp98 <yousef.r1999@gmail.com>
2020-03-20 18:11:28 +0330

    Add Kick and Ban commands

--- dada.py
+++ dada.py
@@ -2,7 +2,7 @@ import discord
 import random
 from discord.ext import commands
 
-token = 'Njg5NTM1MjAwNzQwMjQ1NjIy.XnJmIg.vVSX0t31rqlDzBUwVqnemZyIJSw'
+token = ''
 client = commands.Bot(command_prefix='~')
 
 # !to check bot is up or not
@@ -11,13 +11,24 @@ async def on_ready():
     print('Bot is ready!')
 
 # !the bot commands
-
 # show user ping
 @client.command(aliases=['Ping'])
 async def ping(ctx):
     await ctx.send(f':stopwatch: {round(client.latency * 1000)}ms')
 
-# Response with a random awnseer
+
+# !Kick/Ban commands
+# kick
+@client.command()
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+# Ban
+@client.command()
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+
+# !Response with a random awnseer
 @client.command(aliases=['Estekhare', 'soal', 'Soal'])
 async def estekhare(ctx, *, question):
     responses = [
