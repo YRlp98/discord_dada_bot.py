649b97b6a49bb0164621fc50586804362925248b
yrlp98 <yousef.r1999@gmail.com>
2020-03-20 18:47:07 +0330

    Add unban command

--- dada.py
+++ dada.py
@@ -2,7 +2,7 @@ import discord
 import random
 from discord.ext import commands
 
-token = ''
+token = 'Njg5NTM1MjAwNzQwMjQ1NjIy.XnTT0w.kxJcKEtBsMGh_IhCofISG6SC334'
 client = commands.Bot(command_prefix='~')
 
 # !to check bot is up or not
@@ -22,11 +22,27 @@ async def ping(ctx):
 @client.command()
 async def kick(ctx, member: discord.Member, *, reason=None):
     await member.kick(reason=reason)
+    await ctx.send(f'dada {member.mention} be dalile {reason}, kick shod!')
 
 # Ban
 @client.command()
 async def ban(ctx, member: discord.Member, *, reason=None):
     await member.ban(reason=reason)
+    await ctx.send(f'dada {member.mention} be dalile {reason}, ban shod!')
+
+# Unban
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'dada {user.mention} unban shod!')
+            return
 
 # !Response with a random awnseer
 @client.command(aliases=['Estekhare', 'soal', 'Soal'])
